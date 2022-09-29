abe127afb9d62fe9533374f880839c1589115dc0
Brian-mula <mulatiwangilabrian@gmail.com>
2022-09-30 02:53:57 +0300

    added navbar

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,7 @@
       <h3 class="mt-52 text-3xl font-normal text-black">Welcome to UEAB</h3>
       <h3 class="text-black text-2xl"> File System.</h3>
       <button
+      v-if="!firebaseUser"
       @click="handleLogin"
         type="button"
         class=" mt-8 focus:outline-none text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900"
@@ -19,6 +20,7 @@
 import { doc, getDoc, getFirestore } from "firebase/firestore";
 
 const router=useRouter()
+const firebaseUser=useFirebaseUser()
 
   const db=ref(null)
   onMounted(async()=>{
--- pages/landing.vue
+++ pages/landing.vue
@@ -1,13 +1,13 @@
 <template>
-    <div>
-        <h3 class="text-blue-600">Landing page</h3>
-    </div>
+  <div>
+    <h3 class="text-blue-600">Landing page</h3>
+  </div>
 </template>
 
 <script setup>
-
+definePageMeta({
+  layout: "admin",
+});
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
