3f7d08142b92e692053cb6133943f04428a66351
Brian-mula <mulatiwangilabrian@gmail.com>
2022-09-30 03:31:00 +0300

    added individual schools page

--- composables/store.js
+++ composables/store.js
@@ -1,16 +1,49 @@
-
 import {
-    collection, getDocs,
-    getFirestore
+    collection,
+    doc,
+    getDoc,
+    getDocs,
+    getFirestore, query, where
 } from "firebase/firestore";
 
-export const getAllSchools=async()=> {
-    const db = getFirestore();
-    const schools = [];
-    const schoolSnap = await getDocs(collection(db, "schools"));
-    schoolSnap.forEach((doc) => {
-      schools.push({ ...doc.data(), id: doc.id });
+export const getAllSchools = async () => {
+  const db = getFirestore();
+  const schools = [];
+  const schoolSnap = await getDocs(collection(db, "schools"));
+  schoolSnap.forEach((doc) => {
+    schools.push({ ...doc.data(), id: doc.id });
     //   console.log(doc.id, " => ", doc.data());
-    });
-    return schools;
+  });
+  return schools;
+};
+
+export const getSingleSchool = async (id) => {
+  const db = getFirestore();
+  var school = null;
+
+  const ref = doc(db, "schools", id);
+  const schoolSnap = await getDoc(ref);
+  if (schoolSnap.exists()) {
+    school = schoolSnap.data();
   }
+  return school;
+};
+
+export const getStudentsToSpecificSchool = async (id) => {
+  const db = getFirestore();
+  var school = null;
+  const students = [];
+  const ref = doc(db, "schools", id);
+  const schoolSnap = await getDoc(ref);
+  if (schoolSnap.exists()) {
+    school = schoolSnap.data();
+    const studentRef = collection(db, "students");
+    const q = query(studentRef, where("school", "==", school.name));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      students.push({ ...doc.data(), id: doc.id });
+      console.log(doc.id, " => ", doc.data());
+    });
+  }
+  return students;
+};
