46a2aaf70484c24223be44626cb1fad574108bb8
Nonnie <78481834+nonniekaimburi@users.noreply.github.com>
2023-07-22 09:47:27 +0300

    Update firebaseConfig.client.ts
    trial of deploying on netlify,,,, remember to remove APIKEY after you're done Grace,

--- plugins/firebaseConfig.client.ts
+++ plugins/firebaseConfig.client.ts
@@ -7,23 +7,33 @@ export default defineNuxtPlugin(nuxtApp => {
     // Doing something with nuxtApp
     const config = useRuntimeConfig()
 
+// const firebaseConfig = {
+
+//   apiKey: config.API_KEY,
+
+//   authDomain: config.AUTH_DOMAIN,
+
+//   projectId: config.PROJECT_ID,
+
+//   storageBucket: config.STORAGE_BUCKET,
+
+//   messagingSenderId: config.MESSAGING_SENDER_ID,
+
+//   appId: config.APP_ID,
+//   measurementId: config.MEASUREMENT_ID
+
+// };
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-
-  apiKey: config.API_KEY,
-
-  authDomain: config.AUTH_DOMAIN,
-
-  projectId: config.PROJECT_ID,
-
-  storageBucket: config.STORAGE_BUCKET,
-
-  messagingSenderId: config.MESSAGING_SENDER_ID,
-
-  appId: config.APP_ID,
-  measurementId: config.MEASUREMENT_ID
-
+  apiKey: "AIzaSyDjJNBybOF7g_zeslOezDYSg5czTlnbAS0",
+  authDomain: "ueabfms.firebaseapp.com",
+  databaseURL: "https://ueabfms-default-rtdb.firebaseio.com",
+  projectId: "ueabfms",
+  storageBucket: "ueabfms.appspot.com",
+  messagingSenderId: "361434967495",
+  appId: "1:361434967495:web:f6423a04482ca828be3de2",
+  measurementId: "G-ZJMEHG1NKE"
 };
-
 
 // Initialize Firebase
 
